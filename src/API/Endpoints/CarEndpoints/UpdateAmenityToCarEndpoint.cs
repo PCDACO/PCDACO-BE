3f842ac21119e994b89b5b60c6e44f9942b0cbd7
PersieDaCoder <thinhdpham2510@gmail.com>
2025-01-22 17:05:40 +0700

    feat:update amenity to car

---
+++ src/API/Endpoints/CarEndpoints/UpdateAmenityToCarEndpoint.cs
@@ -0,0 +1,32 @@
+using API.Utils;
+
+using Ardalis.Result;
+
+using Carter;
+
+using MediatR;
+
+using UseCases.UC_Car.Commands;
+
+using IResult = Microsoft.AspNetCore.Http.IResult;
+
+namespace API.Endpoints.CarEndpoints;
+
+public class UpdateAmenityToCarEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPatch("/api/cars/{id:guid}/amenities", Handle);
+    }
+    private async Task<IResult> Handle(ISender sender, Guid id, UpdateAmenityToCarRequest request)
+    {
+        Result result = await sender.Send(new UpdateAmenityToCar.Command(
+            CarId: id,
+            AmenityId: request.AmenityId
+        ));
+        return result.MapResult();
+    }
+    private record UpdateAmenityToCarRequest(
+        Guid[] AmenityId
+    );
+}
