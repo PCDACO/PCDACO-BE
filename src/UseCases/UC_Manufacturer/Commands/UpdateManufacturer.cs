bb36b27ec10db19560797e4621320bd113e1aa7e
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2025-01-24 15:03:10 +0700

    fix: edit namespace and response

--- src/UseCases/UC_Manufacturer/Commands/UpdateManufacturer.cs
+++ src/UseCases/UC_Manufacturer/Commands/UpdateManufacturer.cs
@@ -5,23 +5,32 @@ using Microsoft.EntityFrameworkCore;
 using UseCases.Abstractions;
 using UseCases.DTOs;
 
-namespace UseCases.UC_Manufacturer;
+namespace UseCases.UC_Manufacturer.Commands;
 
 public sealed class UpdateManufacturer
 {
-    public record Command(Guid Id, string ManufacturerName) : IRequest<Result>;
+    public sealed record Command(Guid Id, string ManufacturerName) : IRequest<Result<Response>>;
+
+    public sealed record Response(Guid Id, string Name)
+    {
+        public static Response FromEntity(Manufacturer manufacturer) =>
+            new(manufacturer.Id, manufacturer.Name);
+    };
 
     private class Handler(IAppDBContext context, CurrentUser currentUser)
-        : IRequestHandler<Command, Result>
+        : IRequestHandler<Command, Result<Response>>
     {
-        public async Task<Result> Handle(Command request, CancellationToken cancellationToken)
+        public async Task<Result<Response>> Handle(
+            Command request,
+            CancellationToken cancellationToken
+        )
         {
             if (!currentUser.User!.IsAdmin())
-                return Result.Forbidden("Bạn không có quyền thực hiện thao tác này");
+                return Result.Error("Bạn không có quyền thực hiện thao tác này");
 
             // Check if manufacturer is exist
             Manufacturer? updatingManufacturer = await context.Manufacturers.FirstOrDefaultAsync(
-                m => m.Id == request.Id,
+                m => m.Id == request.Id && !m.IsDeleted,
                 cancellationToken
             );
             if (updatingManufacturer is null)
@@ -31,7 +40,10 @@ public sealed class UpdateManufacturer
             updatingManufacturer.Name = request.ManufacturerName;
             updatingManufacturer.UpdatedAt = DateTimeOffset.UtcNow;
             await context.SaveChangesAsync(cancellationToken);
-            return Result.SuccessWithMessage("Cập nhật hãng xe thành công");
+            return Result.Success(
+                Response.FromEntity(updatingManufacturer),
+                "Cập nhật hãng xe thành công"
+            );
         }
     }
 }
