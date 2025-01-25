067917de48e81ea769dde00613b14561d078e17e
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2025-01-25 16:39:40 +0700

    feat: get all manufacturers

---
+++ src/UseCases/UC_Manufacturer/Queries/GetAllManufacturers.cs
@@ -0,0 +1,54 @@
+using Ardalis.Result;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using UseCases.Abstractions;
+using UseCases.DTOs;
+using UseCases.Utils;
+
+namespace UseCases.UC_Manufacturer.Queries;
+
+public class GetAllManufacturers
+{
+    public sealed record Query(int PageNumber = 1, int PageSize = 10, string keyword = "")
+        : IRequest<Result<OffsetPaginatedResponse<Response>>>;
+
+    public sealed record Response(Guid Id, string Name, DateTimeOffset CreatedAt)
+    {
+        public static Response FromEntity(Manufacturer manufacturer) =>
+            new(manufacturer.Id, manufacturer.Name, GetTimestampFromUuid.Execute(manufacturer.Id));
+    };
+
+    public sealed class Handler(IAppDBContext context)
+        : IRequestHandler<Query, Result<OffsetPaginatedResponse<Response>>>
+    {
+        public async Task<Result<OffsetPaginatedResponse<Response>>> Handle(
+            Query request,
+            CancellationToken cancellationToken
+        )
+        {
+            // Query manufacturers
+            IQueryable<Manufacturer> query = context
+                .Manufacturers.AsNoTracking()
+                .Where(m => m.Name.ToLower().Contains(request.keyword.ToLower()));
+            // Get total result count
+            int count = await query.CountAsync(cancellationToken);
+            // Get manufacturers
+            IEnumerable<Response> manufacturers = await query
+                .OrderByDescending(m => m.Id)
+                .Skip((request.PageNumber - 1) * request.PageSize)
+                .Select(m => Response.FromEntity(m))
+                .ToListAsync(cancellationToken);
+            // Return result
+            return Result.Success(
+                OffsetPaginatedResponse<Response>.Map(
+                    manufacturers,
+                    count,
+                    request.PageNumber,
+                    request.PageSize
+                ),
+                "Get all manufacturers successfully"
+            );
+        }
+    }
+}
