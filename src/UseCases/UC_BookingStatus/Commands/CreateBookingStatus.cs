a04225b9f534374ad133522f9c5ae5c6bb2d8b94
PersieDaCoder <thinhdpham2510@gmail.com>
2025-01-27 13:44:04 +0700

    feat: create booking status
    fix: group all test into 1 container, achive fast execution time

---
+++ src/UseCases/UC_BookingStatus/Commands/CreateBookingStatus.cs
@@ -0,0 +1,50 @@
+
+using Ardalis.Result;
+
+using Domain.Entities;
+
+using FluentValidation;
+
+using MediatR;
+
+using UseCases.Abstractions;
+using UseCases.DTOs;
+
+namespace UseCases.UC_BookingStatus.Commands;
+
+public class CreateBookingStatus
+{
+    public record Command(string Name) : IRequest<Result<Response>>;
+
+    public record Response(
+        Guid Id
+    );
+
+    public class Handler(
+        IAppDBContext context,
+        CurrentUser currentUser
+    ) : IRequestHandler<Command, Result<Response>>
+    {
+        public async Task<Result<Response>> Handle(Command request, CancellationToken cancellationToken)
+        {
+            if (!currentUser.User!.IsAdmin())
+                return Result.Forbidden("Bạn không có quyền thực hiện thao tác này");
+            BookingStatus addingBookingStatus = new()
+            {
+                Name = request.Name
+            };
+            await context.BookingStatuses.AddAsync(addingBookingStatus, cancellationToken);
+            await context.SaveChangesAsync(cancellationToken);
+            return Result.Success(new Response(addingBookingStatus.Id), "Thêm trạng thái đặt phòng thành công");
+        }
+    }
+
+    public class Validator : AbstractValidator<Command>
+    {
+        public Validator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Tên trạng thái không được để trống");
+        }
+    }
+}
