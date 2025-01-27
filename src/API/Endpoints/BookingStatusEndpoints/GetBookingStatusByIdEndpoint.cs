a7521b6ccd8220c81c7866365325fd1253fa2fa8
PersieDaCoder <thinhdpham2510@gmail.com>
2025-01-27 14:37:57 +0700

    feat: get booking status by id
    fix: group all test into 1 container, achive fast execution time

---
+++ src/API/Endpoints/BookingStatusEndpoints/GetBookingStatusByIdEndpoint.cs
@@ -0,0 +1,29 @@
+using API.Utils;
+
+using Ardalis.Result;
+
+using Carter;
+
+using MediatR;
+
+using UseCases.UC_BookingStatus.Queries;
+
+using IResult = Microsoft.AspNetCore.Http.IResult;
+
+namespace API.Endpoints.BookingStatusEndpoints;
+
+public class GetBookingStatusByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/api/users/{id:guid}", Handle)
+        .WithSummary("Get booking status by id")
+        .WithTags("Booking Statuses")
+        .RequireAuthorization();
+    }
+    private async Task<IResult> Handle(ISender sender, Guid id)
+    {
+        Result<GetBookingStatusById.Response> result = await sender.Send(new GetBookingStatusById.Query(id));
+        return result.MapResult();
+    }
+}
