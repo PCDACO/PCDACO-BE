46938cdbb16e345bff8e6af92d49ab82aedf1452
PersieDaCoder <thinhdpham2510@gmail.com>
2025-01-27 14:25:48 +0700

    feat: delete booking status
    fix: group all test into 1 container, achive fast execution time

--- src/UseCases/UC_BookingStatus/Commands/DeleteBookingStatus.cs
+++ src/UseCases/UC_BookingStatus/Commands/DeleteBookingStatus.cs
@@ -1,9 +1,6 @@
-
 using Ardalis.Result;
 
 using Domain.Entities;
-
-using FluentValidation;
 
 using MediatR;
 
@@ -14,11 +11,10 @@ using UseCases.DTOs;
 
 namespace UseCases.UC_BookingStatus.Commands;
 
-public class UpdateBookingStatus
+public class DeleteBookingStatus
 {
     public record Command(
-        Guid Id,
-        string Name
+        Guid Id
     ) : IRequest<Result>;
 
     public class Handler(
@@ -33,22 +29,16 @@ public class UpdateBookingStatus
                 return Result.Forbidden("Bạn không có quyền thực hiện thao tác này !");
             // Check if the booking status is existed
             BookingStatus? updatingBookingStatus = await context.BookingStatuses
+                .Include(bs => bs.Bookings)
                 .FirstOrDefaultAsync(bs => bs.Id == request.Id && !bs.IsDeleted, cancellationToken);
             if (updatingBookingStatus is null)
                 return Result.NotFound("Trạng thái đặt phòng không tồn tại !");
-            // Update booking status
-            updatingBookingStatus.Name = request.Name;
+            if (updatingBookingStatus.Bookings.Count != 0)
+                return Result.Error("Không thể xóa trạng thái đặt phòng đã được sử dụng !");
+            // Delete booking status
+            updatingBookingStatus.Delete();
             await context.SaveChangesAsync(cancellationToken);
-            return Result.SuccessWithMessage("Cập nhật trạng thái chuyến xe thành công !");
-        }
-    }
-
-    public class Validator : AbstractValidator<Command>
-    {
-        public Validator()
-        {
-            RuleFor(x => x.Name)
-                .NotEmpty().WithMessage("Tên trạng thái không được để trống !");
+            return Result.SuccessWithMessage("Xóa trạng thái đặt phòng thành công !");
         }
     }
 }
