c62768589df865224e26773588a35f7fe20887d8
mglongbao <mglongbao@gmail.com>
2025-02-03 16:47:05 +0700

    feat: add IsCarReturned column to Bookings table for car return tracking

---
+++ src/Persistance/Migrations/20250203094639_ConfirmReturnCarForBooking.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfirmReturnCarForBooking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCarReturned",
+                table: "Bookings",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCarReturned",
+                table: "Bookings");
+        }
+    }
+}
