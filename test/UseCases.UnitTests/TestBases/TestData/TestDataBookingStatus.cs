ab30039bc71a30134a1ac0c53863af755c9f0356
mglongbao <mglongbao@gmail.com>
2025-02-03 14:18:31 +0700

    refactor: update CreateBooking method to use statusId and simplify booking status creation

--- test/UseCases.UnitTests/TestBases/TestData/TestDataBookingStatus.cs
+++ test/UseCases.UnitTests/TestBases/TestData/TestDataBookingStatus.cs
@@ -1,4 +1,5 @@
 using Domain.Entities;
+using Domain.Enums;
 using Persistance.Data;
 using UUIDNext;
 
@@ -9,47 +10,14 @@ public static class TestDataBookingStatus
     private static BookingStatus CreateBookingStatus(string statusName) =>
         new() { Id = Uuid.NewDatabaseFriendly(Database.PostgreSql), Name = statusName };
 
-    public static async Task<BookingStatus> CreateTestBookingStatus(
-        AppDBContext dBContext,
-        string statusName
-    )
+    public static async Task<List<BookingStatus>> CreateTestBookingStatuses(AppDBContext dBContext)
     {
-        var bookingStatus = CreateBookingStatus(statusName);
-
-        await dBContext.BookingStatuses.AddAsync(bookingStatus);
-        await dBContext.SaveChangesAsync();
-
-        return bookingStatus;
-    }
-
-    public static async Task<List<BookingStatus>> CreateTestBookingStatuses(
-        AppDBContext dBContext,
-        List<string> statusNames
-    )
-    {
-        var bookingStatuses = statusNames
-            .Select(statusName => CreateBookingStatus(statusName))
+        var statusNames = Enum.GetValues(typeof(BookingStatusEnum))
+            .Cast<BookingStatusEnum>()
+            .Select(x => x.ToString())
             .ToList();
 
-        await dBContext.BookingStatuses.AddRangeAsync(bookingStatuses);
-        await dBContext.SaveChangesAsync();
-
-        return bookingStatuses;
-    }
-
-    public static async Task<List<BookingStatus>> InitializeTestBookingStatuses(
-        AppDBContext dBContext
-    )
-    {
-        List<string> statusNames = new() { "Pending", "Confirmed", "Cancelled" };
-
-        var bookingStatuses = new List<BookingStatus>();
-
-        for (var i = 0; i < statusNames.Count; i++)
-        {
-            var bookingStatus = CreateBookingStatus(statusNames[i]);
-            bookingStatuses.Add(bookingStatus);
-        }
+        var bookingStatuses = statusNames.Select(CreateBookingStatus).ToList();
 
         await dBContext.BookingStatuses.AddRangeAsync(bookingStatuses);
         await dBContext.SaveChangesAsync();
