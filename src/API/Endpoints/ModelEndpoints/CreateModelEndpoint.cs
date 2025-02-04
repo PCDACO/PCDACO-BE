b45590ff62f4d70593a238e8d29ffa47f34bc695
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2025-02-04 08:42:48 +0700

    feat: create model

---
+++ src/API/Endpoints/ModelEndpoints/CreateModelEndpoint.cs
@@ -0,0 +1,31 @@
+using API.Utils;
+using Ardalis.Result;
+using Carter;
+using Infrastructure.Idempotency;
+using MediatR;
+using UseCases.UC_Model.Commands;
+using IResult = Microsoft.AspNetCore.Http.IResult;
+
+namespace API.Endpoints.ModelEndpoints;
+
+public class CreateModelEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/api/models", Handle)
+            .WithSummary("Create a model")
+            .WithTags("Models")
+            .AddEndpointFilter<IdempotencyFilter>()
+            .RequireAuthorization();
+    }
+
+    private async Task<IResult> Handle(ISender sender, CreateModelRequest request)
+    {
+        Result<CreateModel.Response> result = await sender.Send(
+            new CreateModel.Command(request.Name, request.ReleaseDate, request.ManufacturerId)
+        );
+        return result.MapResult();
+    }
+
+    private record CreateModelRequest(string Name, DateTime ReleaseDate, Guid ManufacturerId);
+}
