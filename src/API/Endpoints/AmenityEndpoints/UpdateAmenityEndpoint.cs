425eb2e2d30d21d7b00d92cc0c211146b918009a
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2025-02-06 20:31:01 +0700

    fix update amenity  for updated database

--- src/API/Endpoints/AmenityEndpoints/UpdateAmenityEndpoint.cs
+++ src/API/Endpoints/AmenityEndpoints/UpdateAmenityEndpoint.cs
@@ -1,14 +1,11 @@
 using API.Utils;
-
 using Ardalis.Result;
-
 using Carter;
-
 using MediatR;
-
+using Microsoft.AspNetCore.Mvc;
 using UseCases.UC_Amenity.Commands;
-
 using IResult = Microsoft.AspNetCore.Http.IResult;
+
 namespace API.Endpoints.AmenityEndpoints;
 
 public class UpdateAmenityEndpoint : ICarterModule
@@ -19,12 +16,24 @@ public class UpdateAmenityEndpoint : ICarterModule
             .WithSummary("Update an amenity")
             .WithTags("Amenities")
             .RequireAuthorization();
-
     }
-    private async Task<IResult> Handle(ISender sender, Guid id, UpdateAmenityRequest request)
+
+    private async Task<IResult> Handle(
+        ISender sender,
+        Guid id,
+        [FromForm] UpdateAmenityRequest request
+    )
     {
-        Result result = await sender.Send(new UpdateAmenity.Command(id, request.Name, request.Description));
+        Result result = await sender.Send(
+            new UpdateAmenity.Command(
+                id,
+                request.Name,
+                request.Description,
+                request.Icon?.OpenReadStream()
+            )
+        );
         return result.MapResult();
     }
-    private record UpdateAmenityRequest(string Name, string Description);
+
+    private record UpdateAmenityRequest(string Name, string Description, IFormFile? Icon);
 }
