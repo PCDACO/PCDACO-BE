067b271c26c2bacf84a4c5f564467ed9d2a25451
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2025-02-10 23:38:44 +0700

    fix: cars endpoint for updated database

--- src/UseCases/UC_Car/Commands/UpdateCar.cs
+++ src/UseCases/UC_Car/Commands/UpdateCar.cs
@@ -1,16 +1,10 @@
 using Ardalis.Result;
-
 using Domain.Entities;
 using Domain.Shared;
-
 using FluentValidation;
-
 using MediatR;
-
 using Microsoft.EntityFrameworkCore;
-
 using NetTopologySuite.Geometries;
-
 using UseCases.Abstractions;
 using UseCases.DTOs;
 
@@ -28,6 +22,7 @@ public sealed class UpdateCar
         string Color,
         int Seat,
         string Description,
+        string Address,
         decimal FuelConsumption,
         bool RequiresCollateral,
         decimal PricePerHour,
@@ -47,54 +42,78 @@ public sealed class UpdateCar
     {
         public async Task<Result> Handle(Commamnd request, CancellationToken cancellationToken)
         {
-            if (currentUser.User!.IsAdmin()) return Result.Error("Bạn không có quyền thực hiện chức năng này !");
-            Car? checkingCar = await context.Cars
-                .Include(c => c.EncryptionKey)
+            if (currentUser.User!.IsAdmin())
+                return Result.Error("Bạn không có quyền thực hiện chức năng này !");
+            Car? checkingCar = await context
+                .Cars.Include(c => c.EncryptionKey)
                 .FirstOrDefaultAsync(c => c.Id == request.CarId, cancellationToken);
-            if (checkingCar is null) return Result.Error("Xe không tồn tại");
-            List<Amenity> amenities = await context.Amenities
-                .AsNoTracking()
+            if (checkingCar is null)
+                return Result.Error("Xe không tồn tại");
+            List<Amenity> amenities = await context
+                .Amenities.AsNoTracking()
                 .Where(a => request.AmenityIds.Contains(a.Id))
                 .ToListAsync(cancellationToken);
-            if (amenities.Count != request.AmenityIds.Length) return Result.Error("Một số tiện nghi không tồn tại !");
-            TransmissionType? checkingTransmissionType = await context.TransmissionTypes
-                .FirstOrDefaultAsync(tt => tt.Id == request.TransmissionTypeId && !tt.IsDeleted, cancellationToken);
-            if (checkingTransmissionType is null) return Result.Error("Kiểu hộp số không tồn tại !");
+            if (amenities.Count != request.AmenityIds.Length)
+                return Result.Error("Một số tiện nghi không tồn tại !");
+            TransmissionType? checkingTransmissionType =
+                await context.TransmissionTypes.FirstOrDefaultAsync(
+                    tt => tt.Id == request.TransmissionTypeId && !tt.IsDeleted,
+                    cancellationToken
+                );
+            if (checkingTransmissionType is null)
+                return Result.Error("Kiểu hộp số không tồn tại !");
             // Check if fuel type is exist
-            FuelType? checkingFuelType = await context.FuelTypes
-                .FirstOrDefaultAsync(ft => ft.Id == request.FuelTypeId && !ft.IsDeleted, cancellationToken);
-            if (checkingFuelType is null) return Result.Error("Kiểu nhiên liệu không tồn tại !");
+            FuelType? checkingFuelType = await context.FuelTypes.FirstOrDefaultAsync(
+                ft => ft.Id == request.FuelTypeId && !ft.IsDeleted,
+                cancellationToken
+            );
+            if (checkingFuelType is null)
+                return Result.Error("Kiểu nhiên liệu không tồn tại !");
             // Check if model is exist
-            Model? checkingModel = await context.Models.FirstOrDefaultAsync(m =>
-                m.Id == request.ModelId,
-                cancellationToken);
-            if (checkingModel is null) return Result.Error("Mô hình xe không tồn tại !");
+            Model? checkingModel = await context.Models.FirstOrDefaultAsync(
+                m => m.Id == request.ModelId,
+                cancellationToken
+            );
+            if (checkingModel is null)
+                return Result.Error("Mô hình xe không tồn tại !");
             // Update car amenities
-            await context.CarAmenities.Where(ca => ca.CarId == checkingCar.Id)
+            await context
+                .CarAmenities.Where(ca => ca.CarId == checkingCar.Id)
                 .ExecuteDeleteAsync(cancellationToken);
-            List<CarAmenity> carAmenities = [.. amenities.Select(a => new CarAmenity
-            {
-                CarId = checkingCar.Id,
-                AmenityId = a.Id
-            })];
+            List<CarAmenity> carAmenities =
+            [
+                .. amenities.Select(a => new CarAmenity
+                {
+                    CarId = checkingCar.Id,
+                    AmenityId = a.Id,
+                }),
+            ];
             await context.CarAmenities.AddRangeAsync(carAmenities, cancellationToken);
-            string decryptedKey = keyManagementService.DecryptKey(checkingCar.EncryptionKey.EncryptedKey, encryptionSettings.Key);
-            string encryptedLicensePlate = await aesEncryptionService.Encrypt(request.LicensePlate,
-                                                                            decryptedKey,
-                                                                            checkingCar.EncryptionKey.IV);
+            string decryptedKey = keyManagementService.DecryptKey(
+                checkingCar.EncryptionKey.EncryptedKey,
+                encryptionSettings.Key
+            );
+            string encryptedLicensePlate = await aesEncryptionService.Encrypt(
+                request.LicensePlate,
+                decryptedKey,
+                checkingCar.EncryptionKey.IV
+            );
             // Update car
             checkingCar.ModelId = request.ModelId;
             checkingCar.EncryptedLicensePlate = encryptedLicensePlate;
             checkingCar.Color = request.Color;
             checkingCar.Seat = request.Seat;
             checkingCar.Description = request.Description;
+            checkingCar.Address = request.Address;
             checkingCar.TransmissionTypeId = request.TransmissionTypeId;
             checkingCar.FuelTypeId = request.FuelTypeId;
             checkingCar.FuelConsumption = request.FuelConsumption;
             checkingCar.RequiresCollateral = request.RequiresCollateral;
             checkingCar.PricePerHour = request.PricePerHour;
             checkingCar.PricePerDay = request.PricePerDay;
-            checkingCar.Location = geometryFactory.CreatePoint(new Coordinate((double)request.Longtitude!, (double)request.Latitude!));
+            checkingCar.Location = geometryFactory.CreatePoint(
+                new Coordinate((double)request.Longtitude!, (double)request.Latitude!)
+            );
             checkingCar.UpdatedAt = DateTimeOffset.UtcNow;
             // Save changes
             await context.SaveChangesAsync(cancellationToken);
@@ -106,33 +125,43 @@ public sealed class UpdateCar
     {
         public Validator()
         {
-            RuleFor(x => x.ModelId)
-                .NotEmpty().WithMessage("Phải chọn 1 mô hình xe !");
+            RuleFor(x => x.ModelId).NotEmpty().WithMessage("Phải chọn 1 mô hình xe !");
             RuleFor(x => x.LicensePlate)
-                .NotEmpty().WithMessage("Biển số xe không được để trống !")
-                .MinimumLength(8).WithMessage("Biển số xe không được ít hơn 8 kí tự !")
-                .MaximumLength(11).WithMessage("Biển số xe không được ít hơn 11 kí tự !");
-            RuleFor(x => x.Color)
-                .NotEmpty().WithMessage("Màu sắc không được để trống !");
+                .NotEmpty()
+                .WithMessage("Biển số xe không được để trống !")
+                .MinimumLength(8)
+                .WithMessage("Biển số xe không được ít hơn 8 kí tự !")
+                .MaximumLength(11)
+                .WithMessage("Biển số xe không được ít hơn 11 kí tự !");
+            RuleFor(x => x.Color).NotEmpty().WithMessage("Màu sắc không được để trống !");
             RuleFor(x => x.Seat)
-                .NotEmpty().WithMessage("Số chỗ ngồi không được để trống !")
-                .GreaterThan(0).WithMessage("Số chỗ ngồi phải lớn hơn 0 !")
-                .LessThan(50).WithMessage("Số chỗ ngồi không được quá 50 !");
+                .NotEmpty()
+                .WithMessage("Số chỗ ngồi không được để trống !")
+                .GreaterThan(0)
+                .WithMessage("Số chỗ ngồi phải lớn hơn 0 !")
+                .LessThan(50)
+                .WithMessage("Số chỗ ngồi không được quá 50 !");
             RuleFor(x => x.Description)
-                .MaximumLength(500).WithMessage("Mô tả không được quá 500 ký tự !");
+                .MaximumLength(500)
+                .WithMessage("Mô tả không được quá 500 ký tự !");
+            RuleFor(x => x.Address).NotEmpty().WithMessage("Địa chỉ không được để trống !");
             RuleFor(x => x.FuelConsumption)
-                .NotEmpty().WithMessage("Mức tiêu hao nhiên liệu không được để trống !")
-                .GreaterThan(0).WithMessage("Mức tiêu hao nhiên liệu phải lớn hơn 0 !");
+                .NotEmpty()
+                .WithMessage("Mức tiêu hao nhiên liệu không được để trống !")
+                .GreaterThan(0)
+                .WithMessage("Mức tiêu hao nhiên liệu phải lớn hơn 0 !");
             RuleFor(x => x.PricePerHour)
-                .NotEmpty().WithMessage("Giá thuê theo giờ không được để trống !")
-                .GreaterThan(0).WithMessage("Giá thuê theo giờ phải lớn hơn 0 !");
+                .NotEmpty()
+                .WithMessage("Giá thuê theo giờ không được để trống !")
+                .GreaterThan(0)
+                .WithMessage("Giá thuê theo giờ phải lớn hơn 0 !");
             RuleFor(x => x.PricePerDay)
-                .NotEmpty().WithMessage("Giá thuê theo ngày không được để trống !")
-                .GreaterThan(0).WithMessage("Giá thuê theo ngày phải lớn hơn 0 !");
-            RuleFor(x => x.Latitude)
-                .NotEmpty().WithMessage("Vĩ độ không được để trống !");
-            RuleFor(x => x.Longtitude)
-                .NotEmpty().WithMessage("Kinh độ không được để trống !");
+                .NotEmpty()
+                .WithMessage("Giá thuê theo ngày không được để trống !")
+                .GreaterThan(0)
+                .WithMessage("Giá thuê theo ngày phải lớn hơn 0 !");
+            RuleFor(x => x.Latitude).NotEmpty().WithMessage("Vĩ độ không được để trống !");
+            RuleFor(x => x.Longtitude).NotEmpty().WithMessage("Kinh độ không được để trống !");
         }
     }
 }
