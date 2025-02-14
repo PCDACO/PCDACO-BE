c3e10ecf7196376db1f8b0336412c696e1d3d65f
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2025-02-14 22:44:20 +0700

    feat: get license by user id

---
+++ src/UseCases/UC_License/Queries/GetLicenseByUserId.cs
@@ -0,0 +1,86 @@
+using Ardalis.Result;
+using Domain.Entities;
+using Domain.Shared;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using UseCases.Abstractions;
+using UseCases.DTOs;
+
+namespace UseCases.UC_Driver.Queries;
+
+public static class GetLicenseByUserId
+{
+    public record Query(Guid DriverId) : IRequest<Result<Response>>;
+
+    public record Response(
+        Guid Id,
+        string LicenseNumber,
+        DateTimeOffset ExpirationDate,
+        string? ImageFrontUrl,
+        string? ImageBackUrl
+    )
+    {
+        public static async Task<Response> FromEntityAsync(
+            License license,
+            string masterKey,
+            IAesEncryptionService aesEncryptionService,
+            IKeyManagementService keyManagementService
+        )
+        {
+            string decryptedKey = keyManagementService.DecryptKey(
+                license.EncryptionKey.EncryptedKey,
+                masterKey
+            );
+            string decryptedLicenseNumber = await aesEncryptionService.Decrypt(
+                license.EncryptedLicenseNumber,
+                decryptedKey,
+                license.EncryptionKey.IV
+            );
+            return new(
+                Id: license.Id,
+                LicenseNumber: decryptedLicenseNumber,
+                ExpirationDate: DateTimeOffset.Parse(license.ExpiryDate),
+                ImageFrontUrl: license.LicenseImageFrontUrl,
+                ImageBackUrl: license.LicenseImageBackUrl
+            );
+        }
+    };
+
+    internal class Handler(
+        IAppDBContext context,
+        CurrentUser currentUser,
+        IAesEncryptionService aesEncryptionService,
+        IKeyManagementService keyManagementService,
+        EncryptionSettings encryptionSettings
+    ) : IRequestHandler<Query, Result<Response>>
+    {
+        public async Task<Result<Response>> Handle(
+            Query request,
+            CancellationToken cancellationToken
+        )
+        {
+            if (!currentUser.User!.IsDriver())
+                return Result.Forbidden("Bạn không có quyền thực hiện chức năng này");
+            var license = await context
+                .Licenses.AsNoTracking()
+                .Include(l => l.EncryptionKey)
+                .FirstOrDefaultAsync(
+                    l => l.UserId == request.DriverId && !l.IsDeleted,
+                    cancellationToken
+                );
+
+            if (license is null)
+                return Result.NotFound("Không tìm thấy giấy phép lái xe");
+
+            return Result.Success(
+                await Response.FromEntityAsync(
+                    license: license,
+                    masterKey: encryptionSettings.Key,
+                    aesEncryptionService: aesEncryptionService,
+                    keyManagementService: keyManagementService
+                ),
+                "Lấy thông tin giấy phép lái xe thành công"
+            );
+        }
+    }
+}
