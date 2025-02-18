853da92258e70c92e1de7f9fb52c6935da1b4193
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2025-02-18 11:36:07 +0700

    add validation for update user add response message for invalid password

--- src/Domain/Constants/ResponseMessages.cs
+++ src/Domain/Constants/ResponseMessages.cs
@@ -6,15 +6,19 @@ public class ResponseMessages
     public const string Created = "Tạo thành công";
     public const string Updated = "Cập nhật thành công";
     public const string Deleted = "Xóa thành công";
+
     // PERMISSION
     public const string UnauthourizeAccess = "Bạn không có quyền truy cập";
     public const string ForbiddenAudit = "Bạn không có quyền thực hiện thao tác này";
+
     // USER
     public const string UserNotFound = "Không tìm thấy người dùng";
+    public const string OldPasswordIsInvalid = "Mật khẩu cũ không đúng";
     // GPS DEVICES
     public const string GPSDeviceNotFound = "Không tìm thấy thiết bị GPS";
     public const string GPSDeviceIsExisted = "Đã tồn tại thiết bị GPS";
     public const string GPSDeviceHasCarGPS = "Thiết bị GPS đã được đăng kí sử dụng";
+
     // DEVICE STATUS
     public const string DeviceStatusNotFound = "Không tìm thấy trạng thái thiết bị";
 }
--- src/UseCases/UC_User/Commands/UpdateUser.cs
+++ src/UseCases/UC_User/Commands/UpdateUser.cs
@@ -2,6 +2,7 @@ using Ardalis.Result;
 using Domain.Constants;
 using Domain.Entities;
 using Domain.Shared;
+using FluentValidation;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using UseCases.Abstractions;
@@ -33,6 +34,7 @@ public static class UpdateUser
         EncryptionSettings encryptionSettings
     ) : IRequestHandler<Command, Result<Response>>
     {
+        private readonly IAppDBContext _context = context;
         private readonly CurrentUser _currentUser = currentUser;
         private readonly IAesEncryptionService _aesEncryptionService = aesEncryptionService;
         private readonly IKeyManagementService _keyManagementService = keyManagementService;
@@ -43,7 +45,7 @@ public static class UpdateUser
             CancellationToken cancellationToken
         )
         {
-            var user = await context.Users.FirstOrDefaultAsync(
+            var user = await _context.Users.FirstOrDefaultAsync(
                 x => x.Id == request.Id && !x.IsDeleted,
                 cancellationToken
             );
@@ -71,9 +73,39 @@ public static class UpdateUser
             user.DateOfBirth = request.DateOfBirth;
             user.Phone = encryptedPhone;
 
-            await context.SaveChangesAsync(cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
 
             return Result.Success(Response.FromEntity(user), ResponseMessages.Updated);
+        }
+    }
+
+    public sealed class Validator : AbstractValidator<Command>
+    {
+        public Validator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Tên không được để trống")
+                .MinimumLength(5)
+                .WithMessage("Tên phải có ít nhất 3 ký tự")
+                .MaximumLength(50)
+                .WithMessage("Tên không được quá 50 ký tự");
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Email không được để trống")
+                .EmailAddress()
+                .WithMessage("Email không hợp lệ");
+
+            RuleFor(x => x.Address).NotEmpty().WithMessage("Địa chỉ không được để trống");
+
+            RuleFor(x => x.DateOfBirth)
+                .NotEmpty()
+                .WithMessage("Ngày sinh không được để trống")
+                .LessThan(DateTimeOffset.UtcNow)
+                .WithMessage("Ngày sinh phải nhỏ hơn ngày hiện tại");
+
+            RuleFor(x => x.Phone).NotEmpty().WithMessage("Số điện thoại không được để trống");
         }
     }
 }
