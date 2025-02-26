7c27c35722c70ab193a317a3b136b1c66c88c6a3
mglongbao <mglongbao@gmail.com>
2025-02-26 17:55:14 +0700

    refactor: use Hangfire retries to ensure failed expiration jobs run again

--- src/UseCases/BackgroundServices/Bookings/BookingExpiredJob.cs
+++ src/UseCases/BackgroundServices/Bookings/BookingExpiredJob.cs
@@ -1,4 +1,5 @@
 using Domain.Enums;
+using Hangfire;
 using Microsoft.EntityFrameworkCore;
 using UseCases.Abstractions;
 
@@ -16,13 +17,23 @@ public class BookingExpiredJob(IAppDBContext context)
         if (expiredStatusId == Guid.Empty)
             return;
 
+        // Use Hangfire retries to ensure failed expiration jobs run again
+        BackgroundJob.Enqueue(() => ExpirePendingBookings(expiredStatusId));
+        BackgroundJob.Enqueue(() => ExpireApprovedBookings(expiredStatusId));
+    }
+
+    public async Task ExpirePendingBookings(Guid expiredStatusId)
+    {
         await context
             .Bookings.Where(b =>
                 b.Status.Name == BookingStatusEnum.Pending.ToString()
                 && b.StartTime < DateTimeOffset.UtcNow.AddDays(-30)
             )
             .ExecuteUpdateAsync(b => b.SetProperty(b => b.StatusId, expiredStatusId));
+    }
 
+    public async Task ExpireApprovedBookings(Guid expiredStatusId)
+    {
         await context
             .Bookings.Where(b =>
                 b.Status.Name == BookingStatusEnum.Approved.ToString()
