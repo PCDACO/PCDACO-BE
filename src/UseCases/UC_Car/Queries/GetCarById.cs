6e244294536ca549353ccf175b82e56e08b96d39
PersieDaCoder <thinhdpham2510@gmail.com>
2025-03-02 16:19:43 +0700

    fix/add more field for response

--- src/UseCases/UC_Car/Queries/GetCarById.cs
+++ src/UseCases/UC_Car/Queries/GetCarById.cs
@@ -31,6 +31,10 @@ public class GetCarById
         decimal FuelConsumption,
         bool RequiresCollateral,
         decimal Price,
+        string Terms,
+        string Status,
+        int TotalRented,
+        decimal AverageRating,
         LocationDetail? Location,
         ManufacturerDetail Manufacturer,
         ImageDetail[] Images,
@@ -68,28 +72,31 @@ public class GetCarById
                 car.FuelConsumption,
                 car.RequiresCollateral,
                 car.Price,
+                car.Terms,
+                car.CarStatus.Name,
+                car.CarStatistic.TotalRented,
+                car.CarStatistic.AverageRating,
                 car.GPS == null ? null : new LocationDetail(car.GPS.Location.X, car.GPS.Location.Y),
                 new ManufacturerDetail(car.Model.Manufacturer.Id, car.Model.Manufacturer.Name),
-                [.. car.ImageCars.Select(i => new ImageDetail(i.Id, i.Url))],
+                [.. car.ImageCars.Select(i => new ImageDetail(i.Id, i.Url, i.Type.Name))],
                 [
                     .. car.CarAmenities.Select(a => new AmenityDetail(
                         a.Id,
                         a.Amenity.Name,
-                        a.Amenity.Description
+                        a.Amenity.Description,
+                        a.Amenity.IconUrl
                     )),
                 ]
             );
         }
     };
-
-
     public record LocationDetail(double Longtitude, double Latitude);
 
     public record ManufacturerDetail(Guid Id, string Name);
 
-    public record ImageDetail(Guid Id, string Url);
+    public record ImageDetail(Guid Id, string Url, string Type);
 
-    public record AmenityDetail(Guid Id, string Name, string Description);
+    public record AmenityDetail(Guid Id, string Name, string Description, string Icon);
 
     private sealed class Handler(
         IAppDBContext context,
@@ -105,12 +112,16 @@ public class GetCarById
         {
             Car? gettingCar = await context
                 .Cars
-                .IgnoreQueryFilters()
-                .Include(c => c.Model).ThenInclude(c => c.Manufacturer)
+                .Include(c => c.Owner).ThenInclude(o => o.Feedbacks)
+                .Include(c => c.Model).ThenInclude(o => o.Manufacturer)
                 .Include(c => c.EncryptionKey)
-                .Include(c => c.ImageCars)
+                .Include(c => c.ImageCars).ThenInclude(ic => ic.Type)
+                .Include(c => c.CarStatus)
+                .Include(c => c.CarStatistic)
+                .Include(c => c.TransmissionType)
+                .Include(c => c.FuelType)
+                .Include(c => c.GPS)
                 .Include(c => c.CarAmenities).ThenInclude(ca => ca.Amenity)
-                .Include(c => c.Owner)
                 .Where(c => c.Id == request.Id)
                 .FirstOrDefaultAsync(cancellationToken);
             if (gettingCar is null) return Result.NotFound(ResponseMessages.CarNotFound);
