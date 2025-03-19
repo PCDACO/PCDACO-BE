b401aa99b6c8c9070b7d466e876df3d0f2bd1290
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2025-03-19 15:10:59 +0700

    build: add relationship between InspectionPhoto and InspectionSchedule

--- src/Domain/Entities/InspectionPhoto.cs
+++ src/Domain/Entities/InspectionPhoto.cs
@@ -6,11 +6,15 @@ namespace Domain.Entities;
 
 public class InspectionPhoto : BaseEntity
 {
-    public required Guid InspectionId { get; set; }
+    public Guid? InspectionId { get; set; }
+    public Guid? ScheduleId { get; set; }
     public required InspectionPhotoType Type { get; set; }
     public required string PhotoUrl { get; set; }
     public string Description { get; set; } = string.Empty;
 
     [ForeignKey(nameof(InspectionId))]
-    public CarInspection Inspection { get; set; } = null!;
+    public CarInspection? Inspection { get; set; }
+
+    [ForeignKey(nameof(ScheduleId))]
+    public InspectionSchedule? Schedule { get; set; }
 }
