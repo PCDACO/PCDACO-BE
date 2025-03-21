6843cac4bbc0baa1d216e5ee408ae10d77817214
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2025-03-21 18:30:56 +0700

    db: update database -add InspectionCertificateExpiryDate column to InspectionPhotos table

---
+++ src/Persistance/Migrations/20250321111550_AddInspectionCertificateExpiryDateForInspectionPhoto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInspectionCertificateExpiryDateForInspectionPhoto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "InspectionCertificateExpiryDate",
+                table: "InspectionPhotos",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InspectionCertificateExpiryDate",
+                table: "InspectionPhotos");
+        }
+    }
+}
