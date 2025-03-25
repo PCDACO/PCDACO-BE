06ca6b09bb89a3c024384e504541cd65213004de
PersieDaCoder <thinhdpham2510@gmail.com>
2025-03-25 14:14:37 +0700

    fix: translate types to vietnamese

--- src/Domain/Constants/EntityNames/FuelTypeNames.cs
+++ src/Domain/Constants/EntityNames/FuelTypeNames.cs
@@ -2,8 +2,8 @@ namespace Domain.Constants.EntityNames;
 
 public class FuelTypeNames
 {
-    public const string Gasoline = "Gasoline";
-    public const string Diesel = "Diesel";
-    public const string Electric = "Electric";
+    public const string Gasoline = "Xăng";
+    public const string Diesel = "Dầu";
+    public const string Electric = "Điện";
     public const string Hybrid = "Hybrid";
 }
--- src/Domain/Constants/EntityNames/TransmissionTypeNames.cs
+++ src/Domain/Constants/EntityNames/TransmissionTypeNames.cs
@@ -2,6 +2,6 @@ namespace Domain.Constants.EntityNames;
 
 public class TransmissionTypeNames
 {
-    public const string Manual = "Manual";
-    public const string Automatic = "Automatic";
+    public const string Manual = "Số Sàn";
+    public const string Automatic = "Số Tự Động";
 }
