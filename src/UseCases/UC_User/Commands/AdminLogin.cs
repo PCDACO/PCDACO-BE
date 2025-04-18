f8008875c22ea805b5c04aa95887a810b63e25a1
Nguyen Huynh Anh Tai <144357738+NguyenHuynhAnhTai@users.noreply.github.com>
2025-04-18 13:57:26 +0700

    fix: Update AdminLogin handler to correctly return error status for invalid password

--- src/UseCases/UC_User/Commands/AdminLogin.cs
+++ src/UseCases/UC_User/Commands/AdminLogin.cs
@@ -1,12 +1,7 @@
-
 using Ardalis.Result;
-
 using Domain.Entities;
-
 using MediatR;
-
 using Microsoft.EntityFrameworkCore;
-
 using UseCases.Abstractions;
 using UseCases.Utils;
 
@@ -15,22 +10,25 @@ namespace UseCases.UC_User.Commands;
 public class AdminLogin
 {
     public record Command(string Email, string Password) : IRequest<Result<Response>>;
+
     public record Response(string AccessToken, string RefreshToken);
-    public class Handler(
-        IAppDBContext context,
-        TokenService tokenService
-    ) : IRequestHandler<Command, Result<Response>>
+
+    public class Handler(IAppDBContext context, TokenService tokenService)
+        : IRequestHandler<Command, Result<Response>>
     {
-        public async Task<Result<Response>> Handle(Command request, CancellationToken cancellationToken)
+        public async Task<Result<Response>> Handle(
+            Command request,
+            CancellationToken cancellationToken
+        )
         {
             User? user = await context
                 .Users.AsNoTracking()
-                .FirstOrDefaultAsync(
-                    u => u.Email == request.Email && u.Password == request.Password.HashString(),
-                    cancellationToken
-                );
+                .FirstOrDefaultAsync(u => u.Email == request.Email, cancellationToken);
             if (user is null)
                 return Result.NotFound("Không tìm thấy thông tin người dùng");
+
+            if (user.Password != request.Password.HashString())
+                return Result.Error("Sai mật khẩu");
             string accessToken = tokenService.GenerateAccessToken(user);
             string refreshToken = tokenService.GenerateRefreshToken();
             RefreshToken addingRefreshToken = new()
@@ -41,10 +39,7 @@ public class AdminLogin
             };
             await context.RefreshTokens.AddAsync(addingRefreshToken, cancellationToken);
             await context.SaveChangesAsync(cancellationToken);
-            return Result.Success(
-                new Response(accessToken, refreshToken),
-                "Đăng nhập thành công"
-            );
+            return Result.Success(new Response(accessToken, refreshToken), "Đăng nhập thành công");
         }
     }
 }
