809dcd225539c0e46296792ea8e8bfa4b4ce2d6c
mglongbao <mglongbao@gmail.com>
2025-04-26 18:42:59 +0700

    feat: add ExtensionAmount and IsExtensionPaid columns to Bookings table

---
+++ src/Persistance/Migrations/20250426085239_AddExtendDateBooking.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExtendDateBooking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "ExtensionAmount",
+                table: "Bookings",
+                type: "numeric",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsExtensionPaid",
+                table: "Bookings",
+                type: "boolean",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExtensionAmount",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "IsExtensionPaid",
+                table: "Bookings");
+        }
+    }
+}
